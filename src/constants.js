8ab5454ea47777f883af84042eddef2595a3b580
Darron Jennings <darronj@outlook.com>
2021-02-13 17:04:41 -0600

    updating

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+const THEME = {
+  global: {
+    colors: {
+      brand: "rgba(255, 203, 5, 0.49)", //"#22BBE6",
+    },
+    font: {
+      family: "Roboto",
+      size: "18px",
+      height: "20px",
+    },
+  },
+};
+
+module.exports = {
+  THEME,
+};
